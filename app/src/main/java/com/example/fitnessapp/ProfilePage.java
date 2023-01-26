a92f7bbb5a2013aea148403587929904d237d00b
darshan-1204 <dabhidarshan2004@gmail.com>
2023-01-26 18:28:24 +0530

    commit

--- app/src/main/java/com/example/fitnessapp/ProfilePage.java
+++ app/src/main/java/com/example/fitnessapp/ProfilePage.java
@@ -1,6 +1,9 @@
 package com.example.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,22 +12,52 @@ import android.widget.Button;
 
 public class ProfilePage extends AppCompatActivity {
 
-    Button btn_history;
+    Button badge, history;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+        loadFragment(new BadgeFragment());
 
-        btn_history = findViewById(R.id.btn_history);
+        badge = findViewById(R.id.badgeButton);
+        history = findViewById(R.id.historyButton);
 
-        btn_history.setOnClickListener(new View.OnClickListener() {
+        badge.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Intent intent = new Intent(ProfilePage.this,HIstory.class);
-                finish();
-                startActivity(intent);
+                loadFragment(new BadgeFragment());
+            }
+        });
+        history.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadFragment(new HistoryFragment());
             }
         });
     }
+
+    void loadFragment(Fragment fragment) {
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragFrame, fragment);
+        transaction.commit();
+    }
 }
+
+/*
+            var profileFragment: ProfileFragment = ProfileFragment()
+            var manager: FragmentManager = supportFragmentManager
+            var transaction: FragmentTransaction = manager.beginTransaction()
+            transaction.replace(R.id.frame,profileFragment)
+            transaction.commit()
+
+ //For Loading Fragment Function
+    fun loadFragment(fragment: Fragment) {
+
+        var transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame, fragment)
+        transaction.commit()
+    }
+}
+ */
